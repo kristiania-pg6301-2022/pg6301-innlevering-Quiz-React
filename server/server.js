d2bb75ecea5e3a06e8d57ac8678ec6631c82e29d
mojibhashemi123 <muha006@student.kristiania.no>
2022-03-03 15:45:00 +0100

    add prettier and husky

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
-import express from "express"
-
+import express from "express";
 
 const app = express();
 
 app.get("/api/question", (req, res, next) => {
-    res.json({question: "What was the last thing you read?"});
-})
+  res.json({ question: "What was the last thing you read?" });
+});
 
 app.post("/api/question", (req, res, next) => {
-    res.sendStatus(401);
-})
+  res.sendStatus(401);
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`started on http://localhost:${server.address().port}`)
+  console.log(`started on http://localhost:${server.address().port}`);
 });
