87ae7be283999b1a6e67add72a9f3e52829c0266
mojibhashemi123 <muha006@student.kristiania.no>
2022-03-04 21:47:27 +0100

    React app for Heroku (fix)

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome to Quiz</h1>
+      <h3>Choice</h3>
+    </div>
+  );
+}
+
+ReactDOM.render(<FrontPage />, document.getElementById("app"));
