32386caae97ba9cb8ccd70260652ddef53c40473
mojibhashemi123 <muha006@student.kristiania.no>
2022-03-04 21:56:54 +0100

    Commit on heroku

--- client/index.jsx
+++ client/index.jsx
@@ -4,8 +4,14 @@ import ReactDOM from "react-dom";
 function FrontPage() {
   return (
     <div>
-      <h1>Welcome to Quiz</h1>
-      <h3>Choice</h3>
+      <div>
+        <h1>Welcome to Quiz</h1>
+        <h3>Choice</h3>
+      </div>
+      <div>
+        <button>Take quiz</button>
+        <button>Result</button>
+      </div>
     </div>
   );
 }
