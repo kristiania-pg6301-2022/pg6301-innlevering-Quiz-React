71194cb2400c441c34bc53df215161feefbaa7b3
mojibhashemi123 <muha006@student.kristiania.no>
2022-03-07 21:15:37 +0100

    Testing fetch

--- client/index.jsx
+++ client/index.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function FrontPage() {
+import { FrontPage } from "./components/FrontPage";
+
+function Quiz() {
+  const [user, setUser] = useState();
+  useEffect(async () => {
+    const res = await fetch("api/question");
+    setUser(await res.json());
+  }, []);
+  return <div>{user ? <div>{user.question}</div> : <h1>not found</h1>}</div>;
+}
+
+function Application() {
   return (
-    <div>
-      <div>
-        <h1>Welcome to Quiz</h1>
-        <h3>Choice</h3>
-      </div>
-      <div>
-        <button>Take quiz</button>
-        <button>Result</button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path={"/newQuestion"} element={<Quiz />} />
+        <Route path={"/result"} element={<h1>hei</h1>} />
+        <Route path="/*" element={<h1>Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-ReactDOM.render(<FrontPage />, document.getElementById("app"));
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import * as path from "path";
+
+import { isCorrectAnswer, Questions, randomQuestion } from "./questions.js";
 
 const app = express();
 
@@ -6,11 +9,23 @@ app.get("/api/question", (req, res, next) => {
   res.json({ question: "What was the last thing you read?" });
 });
 
+app.get("/question/random", (req, res) => {
+  const question = randomQuestion();
+  res.json(question);
+});
+
 app.post("/api/question", (req, res, next) => {
   res.sendStatus(401);
 });
 
-app.use(express.static("../client/dist"));
+app.use(express.static(path.resolve("../client/dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`started on http://localhost:${server.address().port}`);
